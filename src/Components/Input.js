de0dd62595640e9d044373ce8d38f21b8d6ef192
william <wjeffharris@outlook.com>
2020-01-02 20:41:56 -0600

    user is now able to send messages

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
 
 export default class Input extends Component {
+   
+    state = {
+        text:""
+    }
+    onChange(e) {
+        this.setState({text: e.target.value});
+      }
+    onSubmit(e) {
+        e.preventDefault();
+        this.setState({text: ""});
+        this.props.onSendMessage(this.state.text);
+      }
     render() {
         return (
-            <div>
-                
-            </div>
+            <div className="Input">
+            <form onSubmit={e => this.onSubmit(e)}>
+              <input
+                onChange={e => this.onChange(e)}
+                value={this.state.text}
+                type="text"
+                placeholder="Enter your message and press ENTER"
+                autofocus="true"
+              />
+              <button>Send</button>
+            </form>
+          </div>
         )
     }
 }
